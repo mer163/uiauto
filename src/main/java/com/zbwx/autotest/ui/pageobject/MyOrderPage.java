b7aa8742fc00433a51d91c77a731bf24fa488877
Ds <dsdy@163.com>
2018-08-14 16:28:14 +0800

    添加我的订单页面

---
+++ src/main/java/com/zbwx/autotest/ui/pageobject/MyOrderPage.java
@@ -0,0 +1,28 @@
+package com.zbwx.autotest.ui.pageobject;
+
+import com.zbwx.autotest.ui.base.BasePage;
+import com.zbwx.autotest.ui.base.BaseTest;
+
+import atx.client.internal.FindElementBy;
+import atx.client.model.AndroidElement;
+
+//我的订单
+public class MyOrderPage extends BasePage {
+	@FindElementBy(text="消息")
+	public AndroidElement mMessage_Button;//消息按钮
+
+	@FindElementBy(id= "id/leftBtn")
+	public AndroidElement mReturn_Button;//返回按钮
+	
+	@FindElementBy(text="果礼镍")
+	public AndroidElement mCommodity_Ni;//果礼镍
+	
+
+	public MyOrderPage(){
+		super();
+	}
+
+	public static MyOrderPage verify(){
+		return  new MyOrderPage();
+	}
+}
