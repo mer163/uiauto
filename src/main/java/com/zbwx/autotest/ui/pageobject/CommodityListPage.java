b2f00ad2dd208d4e41e965147206693d0c7f778c
Ds <dsdy@163.com>
2018-08-14 16:26:02 +0800

    添加商品列表

---
+++ src/main/java/com/zbwx/autotest/ui/pageobject/CommodityListPage.java
@@ -0,0 +1,27 @@
+package com.zbwx.autotest.ui.pageobject;
+
+import com.zbwx.autotest.ui.base.BasePage;
+
+import atx.client.internal.FindElementBy;
+import atx.client.model.AndroidElement;
+
+//商品列表页面
+public class CommodityListPage extends BasePage {
+	
+	@FindElementBy(text="消息")//消息按钮
+	public AndroidElement mMessage_Button;
+
+	@FindElementBy(id= "id/leftBtn")//返回按钮
+	public AndroidElement mReturn_Button;
+	
+	@FindElementBy(text="果礼镍")//果礼镍
+	public AndroidElement mCommodity_Ni;
+
+	public CommodityListPage(){
+		super();
+	}
+
+	public static CommodityListPage verify(){
+		return  new CommodityListPage();
+	}
+}
