ebc68515fae02b6bcc7413ed1f7bd49c362a70a6
hmh <minghuan.huang@zbwxkj.com>
2018-08-15 19:12:34 +0800

    更新消息方法页面

--- src/main/java/com/zbwx/autotest/ui/pageobject/homeobject/HomePageNotifications.java
+++ src/main/java/com/zbwx/autotest/ui/pageobject/homeobject/HomePageNotifications.java
@@ -1,16 +1,22 @@
 package com.zbwx.autotest.ui.pageobject.homeobject;
 
+
 import atx.client.internal.FindElementBy;
 import atx.client.model.AndroidElement;
 
 
+
+
+
+import com.zbwx.autotest.ui.base.BaseAction;
+import com.zbwx.autotest.ui.base.BasePage;
 import com.zbwx.autotest.ui.base.BaseTest;
 /**
  * 消息
  * @author zbwx
  *
  */
-public class HomePageNotifications extends BaseTest {
+public class HomePageNotifications extends BasePage {
  
 	@FindElementBy(id= "id/leftBtn")	
 	public AndroidElement mMessageBackBtn;
@@ -35,5 +41,18 @@ public class HomePageNotifications extends BaseTest {
 	public static HomePageNotifications verify(){
 		return  new HomePageNotifications();
 	}
-	
+	public void mClickNoti() throws Exception{
+		this.mNoticeContainer.click();
+		Thread.sleep(2000);
+		((AndroidElement) BaseAction.driver.findElementById(BaseTest.mAppMainPackage +":id/newcfd_message_back")).click();
+		Thread.sleep(2000);
+		
+		this.mTixianContainer.click();
+		Thread.sleep(2000);
+		((AndroidElement) BaseAction.driver.findElementById(BaseTest.mAppMainPackage +":id/newcfd_message_back")).click();
+		Thread.sleep(2000);
+		this.mDealContainer.click();
+		Thread.sleep(2000);
+		((AndroidElement) BaseAction.driver.findElementById(BaseTest.mAppMainPackage +":id/newcfd_message_back")).click();
+	}
 }
