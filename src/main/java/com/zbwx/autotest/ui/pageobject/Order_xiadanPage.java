359078817b722d1331841b5b508d99e2f65de047
Ds <dsdy@163.com>
2018-08-15 17:17:44 +0800

    更新下单页面

--- src/main/java/com/zbwx/autotest/ui/pageobject/Order_xiadanPage.java
+++ src/main/java/com/zbwx/autotest/ui/pageobject/Order_xiadanPage.java
@@ -1,13 +1,16 @@
 package com.zbwx.autotest.ui.pageobject;
 
+import com.zbwx.autotest.ui.base.BaseAction;
 import com.zbwx.autotest.ui.base.BasePage;
-import com.zbwx.autotest.ui.utils.Assertion;
 
 import atx.client.internal.FindElementBy;
 import atx.client.model.AndroidElement;
 
 //下单页面
 public class Order_xiadanPage extends BasePage {
+	
+	DSa_Method ds = new DSa_Method();
+
 
 	@FindElementBy(id="id/rightBtn")//消息按钮
 	public AndroidElement mMessageButton;
@@ -45,7 +48,8 @@ public class Order_xiadanPage extends BasePage {
 	@FindElementBy(text="2.4")//止损2.4
 	public AndroidElement mStopLoss_2_4;
 	
-	public Order_xiadanPage(){
+	
+	protected Order_xiadanPage(){
 		super();
 	}
 
@@ -53,7 +57,12 @@ public class Order_xiadanPage extends BasePage {
 		return  new Order_xiadanPage();
 	}
 
-	DSa_Method ds = new DSa_Method();
+	/**
+	 * 点击消息按钮
+	 */
+	public void mClickMessage(){
+		this.mMessageButton.click();
+	}
 	
 	/**
 	 * 下单页面，获取当前购买手数
@@ -70,22 +79,133 @@ public class Order_xiadanPage extends BasePage {
 	 */
 	public void mRecoveryXiaDanPgae() throws Exception{
 		ds.mClickReturnButton();
-		mOpenOrder_xiadan();
+		ds.mOpenOrder_xiadan();
 	}
 	
 	/**
-	 * 首页开始,点击‘现价定购’进入下单页面(果礼镍)
-	 * @throws Exception
+	 * 下单页面切换商品（果礼铜）
 	 */
-	public void mOpenOrder_xiadan() throws Exception{
-		ds.mOpenOrderPage();
-		OrderPage order = new OrderPage();
-		order.mPresentPriceOrder.click();
-		Assertion.VerityTextPresentPrecision("下单", "是否已进入下单页面...");
-		Assertion.VerityTextPresentPrecision("果礼镍", "商品是否为果礼镍...");	
+	public void mSelectOrder(){
+		mSelectOrderButton.click();
+		BaseAction.driver.findElementByName("果礼铜").click();
 	}
 	
-	public void mClearAndReinput(String id ){
-		
+	/**
+	 * 下单页面点击选择卡券
+	 */
+	public void mClickSelectCoupon(){
+		this.mSelectCoupon.click();
+	}
+	
+	/**
+	 * 下单页面下，进行选择商品种类（果礼镍0.08吨）
+	 */
+	public void mSelectType(){
+		this.mSelectType.click();
+		BaseAction.driver.findElementByName("果礼镍0.08吨").click();
+	}
+	
+	/**
+	 * 清除默认购买手数后，手动输入购买手数
+	 * @param num：要输入的手数
+	 */
+	public void mSendOrderNumber(String num){
+		this.mOrderNumber.click();
+		BaseAction.device.clearText("111");
+		this.mOrderNumber.click();
+		BaseAction.device.sendText(num);
+	}
+	
+	/**
+	 * 点击减少手数按钮
+	 */
+	public void mClickOrderNumber_subtract(){
+		this.mOrderNumber_subtract.click();
+	}
+	
+	/**
+	 * 点击增加手数按钮
+	 */
+	public void mClickOrderNumber_add(){
+		this.mOrderNumber_add.click();
+	}
+	
+	/**
+	 * 设置止盈为16.0（最大止盈）
+	 */
+	public void mSetStopProfit_160(){
+		this.mStopProfit_16_0.click();
+	}
+	
+	/**
+	 * 设置止损为2.4
+	 */
+	public void mSetStopLoss_24(){
+		this.mStopLoss_2_4.click();
+	}
+	
+	/**
+	 * 点击充值按钮
+	 */
+	public void mClickRecharge(){
+		this.mRechargeButton.click();
+	}
+	
+	/**
+	 * 等待可用资产加载显示
+	 * @throws Exception 
+	 */
+	public void mWaitAvailableAssets() throws Exception{
+		String str1 = "--.--";
+		String str2;
+		for (int i = 0; i < 9; i++) {
+			BaseAction.driver.sleep(1000);
+			str2 = ds.mGetYuE();
+			if(str1.equals(str2)){
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * 设置购买手数为1
+	 */
+	public void mSetOrderNum_1(){
+		mSendOrderNumber("1");
+		for(int i=0 ;i<9 ;i++){
+			mClickOrderNumber_subtract();
+		}
+	}
+	
+	/**
+	 * 设置购买为1手后，下单
+	 * @throws Exception 
+	 */
+	public void mXiaDan() throws Exception{
+		//避免直接购买最大手数以导致第二次测试时无法购买，所以设置为只购买一手
+		mSetOrderNum_1();
+		this.mPresentPriceOrder.click();		
+	}
+	
+	/**
+	 * 确认购买
+	 */
+	public void mOkPurchase(){
+		BaseAction.driver.findElementById("com.ylmall.app.ui:id/dialog_ok").click();
+	}
+	
+	/**
+	 * 取消购买
+	 */
+	public void mNOPurchase(){
+		BaseAction.driver.findElementById("com.ylmall.app.ui:id/dialog_cancle").click();
+	}
+	
+	/**
+	 * 切换到结算价高购买下单页面
+	 */
+	public void mswitchSettlementPriceOrder(){
+		ds.mClickReturnButton();
+		this.mSettlementPriceOrder.click();
 	}
 }
