8cd4a113cf34244d8f179fd435684e6f29207f15
Ds <dsdy@163.com>
2018-08-17 17:15:58 +0800

    更新脚本

--- src/test/java/com/zbwx/autotest/ui/orderpage/TestOrder.java
+++ src/test/java/com/zbwx/autotest/ui/orderpage/TestOrder.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.zbwx.autotest.ui.base.BaseTest;
+import com.zbwx.autotest.ui.pageobject.HomePage;
 import com.zbwx.autotest.ui.pageobject.orderobject.DSa_Method;
 import com.zbwx.autotest.ui.pageobject.orderobject.OrderPage;
 import com.zbwx.autotest.ui.utils.Assertion;
@@ -21,20 +22,21 @@ public class TestOrder extends BaseTest {
 	
 	@BeforeClass//所有方法开始前执行
 	public void beforeClass() throws Exception {
-		//检查当前是否处于下单页面,如果不是,则返回首页后点击‘现价定购’进入下单页面(果礼镍)
-		ds.mCheckTestOrderPage();
+		
+		BaseAction.position.waitForElement(ElementAttribs.TEXT, "首页", 10000);
 	}
 	
 	@BeforeMethod//每个方法执行前执行
 	public void beforeMethod() throws Exception{
-		//检查当前是否处于下单页面,如果不是,则返回首页后点击‘现价定购’进入下单页面(果礼镍)
-		ds.mCheckTestOrderPage();
+		//
+		HomePage.verify().order.click();
+		OrderPage.verify().mCommodity_1.click();
 	}
 
 	@AfterMethod//每个方法执行完后执行
 	public void aftermthod() throws Exception{
 		//恢复下单页面初始(返回上一页面重新进入)
-		ds.mRecoveryOrderPage();
+		HomePage.verify().home_bottom.click();
 	}
 
 	@AfterClass//所有测试方法执行完后执行
@@ -42,63 +44,69 @@ public class TestOrder extends BaseTest {
 		
 	}
 	
-	@Test(description = "点击‘客服’按钮")
+	@Test(description = "进入‘客服’页面")
 	public void testClickCustomerService() throws Exception{
 		//点击客服按钮
-		OrderPage.verify().mClickCustomerService();
+		OrderPage.verify().mCustomerService.click();
 		Assertion.VerityTextPresentPrecision("客服中心", "是否进入客服中心页面");
-		ds.mClickReturnButton();
+		//返回上一页面
+		BaseAction.mReturn();
 	}
 	
-	@Test(description = "点击消息按钮")
+	@Test(description = "进入‘消息’页面")
 	public void testClickMessage() throws Exception{
 		//点击消息按钮
-		OrderPage.verify().mClickMessage();
+		OrderPage.verify().mMessageButton.click();
 		Assertion.VerityTextPresentPrecision("消息中心", "是否进入消息中心...");
-		ds.mClickReturnButton();
+		//返回上一页面
+		BaseAction.mReturn();
 	}
 	
 	@Test(description = "定购页面切换商品")
 	public void testSwitchCommodity() throws Exception{
-		//切换商品（果礼铜）
-		ds.mSwitchCu();
-		Assertion.VerityTextPresentPrecision("果礼铜0.02吨", "验证是否成功切换商品...");
+		//获取当前商品规格名称
+		String str1 = OrderPage.verify().mTypeName.getText();
+		//切换第二个商品
+		OrderPage.verify().mCommodity_2.click();
+		//获取第二个商品的商品规格名称
+		String str2 = OrderPage.verify().mTypeName.getText();
+		Assertion.VerityBoolean(str1.equals(str2), false, "验证是否成功切换商品...");
 	}
 	
 	@Test(description = "定购首页K线选择M5")
 	public void testK_lineSelectM5() throws Exception{
 		//点击K线_M5
-		ds.mClickKX("M5");
+		OrderPage.verify().mK_M5.click();
 		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "K线显示切换");
 	}
 	
 	@Test(description = "定购首页K线选择M15")
 	public void testK_lineSelectM15() throws Exception{
 		//点击K线_M15
-		ds.mClickKX("M15");
+		OrderPage.verify().mK_M15.click();
 		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "K线显示切换");
 	}
 	
 	@Test(description = "定购首页K线选择H1")
 	public void testK_lineSelectH1() throws Exception{
 		//点击K线_H1
-		ds.mClickKX("H1");
+		OrderPage.verify().mK_H1.click();
 		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "K线显示切换");
 	}
 	
 	@Test(description = "定购首页K线选择D1")
 	public void testK_lineSelectD1() throws Exception{
 		//点击K线_D1
-		ds.mClickKX("D1");
+		OrderPage.verify().mK_D1.click();
 		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "K线显示切换");
 	}
 	
 	@Test(description = "定购首页K线选择分时线")
 	public void testK_lineSelectTimeshare() throws Exception{
 		//点击K线_M5
-		ds.mClickKX("M5");
+		OrderPage.verify().mK_M15.click();
 		//点击K线_分时线
-		ds.mClickKX("分时线");
+		OrderPage.verify().mK_Timeshare.click();
 		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_time_hour_1", 3000), true, "K线显示切换");
 	}
 	
@@ -108,44 +116,48 @@ public class TestOrder extends BaseTest {
 //		
 //	}
 	
-	@Test(description = "点击充值按钮")
+	@Test(description = "进入‘充值’页面")
 	public void testClickRecharge() throws Exception{
-		OrderPage.verify().mClickRecharge();
+		OrderPage.verify().mRechargeButton.click();
 		Assertion.VerityTextPresentPrecision("充值查询", "跳转充值页面...");
-		ds.mClickReturnButton();		
+		//返回上一页面
+		BaseAction.mReturn();	
 	}
 	
-	@Test(description = "点击我的订单按钮")
+	@Test(description = "进入‘我的订单’")
 	public void testClickMyOrder() throws Exception{
-		OrderPage.verify().mClickMyOrder();
+		OrderPage.verify().mMyOrder.click();
 		Assertion.VerityTextPresentPrecision("我的订单", "跳转我的订单页面...");
-		ds.mClickReturnButton();
+		//返回上一页面
+		BaseAction.mReturn();
 	}
 	
 	@Test(description = "定购页面选择商品种类")
 	public void testSelectType() throws Exception{
-		//选择商品规格为80KG
-		OrderPage.verify().mClickSelectType("80");
+		//选择商品规格为第二个
+		OrderPage.verify().mType_2.click();
 		Assertion.VerityTextPresentPrecision("果礼镍0.08吨", "选择80KG...");
-		//选择商品规格为400KG
-		OrderPage.verify().mClickSelectType("400");
+		//选择商品规格为第三个
+		OrderPage.verify().mType_3.click();
 		Assertion.VerityTextPresentPrecision("果礼镍0.4吨", "选择400KG...");
-		//选择商品规格为10KG
-		OrderPage.verify().mClickSelectType("10");
+		//选择商品规格为第一个
+		OrderPage.verify().mType_1.click();
 		Assertion.VerityTextPresentPrecision("果礼镍0.01吨", "选择10KG...");
 	}
 	
-	@Test(description = "点击现价购买按钮")
+	@Test(description = "进入‘下单’页面_点击‘现价定购’")
 	public void testPresentPriceOrder() throws Exception{
 		//点击现价购买按钮
-		ds.mClickPresentPrice();
-		Assertion.VerityTextPresentPrecision("下单", "是否进入下单页面");
+		OrderPage.verify().mPresentPriceOrder.click();
+		Assertion.VerityTextPresentPrecision("下单", "是否进入下单页面...");
+		BaseAction.mReturn();
 	}
 	
-	@Test(description = "点击结算价购买按钮")
+	@Test(description = "进入‘下单’页面_点击‘结算价定购’")
 	public void testSettlementPriceOrder() throws Exception{
 		//点击结算价购买按钮
-		ds.mClickSettlementPrice();
-		Assertion.VerityTextPresentPrecision("下单", "是否进入下单页面");
+		OrderPage.verify().mSettlementPriceOrder.click();
+		Assertion.VerityTextPresentPrecision("下单", "是否进入下单页面...");
+		BaseAction.mReturn();
 	}
 }
--- src/test/java/com/zbwx/autotest/ui/orderpage/TestQuotes.java
+++ src/test/java/com/zbwx/autotest/ui/orderpage/TestQuotes.java
@@ -2,6 +2,7 @@ package com.zbwx.autotest.ui.orderpage;
 
 import com.zbwx.autotest.ui.base.BaseAction;
 import com.zbwx.autotest.ui.base.BaseTest;
+import com.zbwx.autotest.ui.pageobject.HomePage;
 import com.zbwx.autotest.ui.pageobject.orderobject.DSa_Method;
 import com.zbwx.autotest.ui.pageobject.orderobject.OrderPage;
 import com.zbwx.autotest.ui.pageobject.orderobject.QuotesPage;
@@ -21,20 +22,21 @@ public class TestQuotes extends BaseTest {
 	
 	@BeforeClass//所有方法开始前执行
 	public void beforeClass() throws Exception {
-		//检查当前是否处于行情页面,如果不是,则返回首页后重新进入果礼镍的行情页面
-		ds.mCheckQuotesPage();
+		BaseAction.position.waitForElement(ElementAttribs.TEXT, "首页", 10000);
 	}
 	
 	@BeforeMethod//每个方法执行前执行
 	public void beforeMethod() throws Exception{
 		//检查当前是否处于行情页面,如果不是,则返回首页后重新进入果礼镍的行情页面
-		ds.mCheckQuotesPage();
+		//ds.mCheckQuotesPage();
+		HomePage.verify().tv_homeNie.click();
 	}
 
 	@AfterMethod//每个方法执行完后执行
 	public void aftermthod() throws Exception{
 		//恢复下单页面初始(返回上一页面重新进入)
-		ds.mRecoveryOrderPage();
+		//ds.mRecoveryOrderPage();
+		BaseAction.mReturn();
 	}
 
 	@AfterClass//所有测试方法执行完后执行
@@ -45,63 +47,64 @@ public class TestQuotes extends BaseTest {
 	@Test(description = "行情页面切换商品")
 	public void testSwitchCommodity() throws Exception{
 		//获取当前商品价格
-		String e1 = ds.mGetTextById("com.ylmall.app.ui:id/mogen_price");
-		//切换商品到果礼铜
-		ds.mSwitchCu();
+		String e1 = QuotesPage.verify().mMogenPrice.getText();
+		//切换商品到第二个商品
+		QuotesPage.verify().mCommodity_2.click();
 		//获取切换商品后价格
-		String e2= ds.mGetTextById("com.ylmall.app.ui:id/mogen_price");
+		String e2= QuotesPage.verify().mMogenPrice.getText();
 		Assertion.VerityBoolean(e1.equals(e2), false, "成功切换商品后价格会有变动...");
 	}
 	
-	@Test(description = "行情页面点击消息按钮")
+	@Test(description = "进入‘消息’页面")
 	public void testClickMessage() throws Exception{
-		OrderPage.verify().mClickMessage();
+		//点击消息按钮
+		QuotesPage.verify().mMessageButton.click();BaseAction.driver.sleep(2000);
 		Assertion.VerityTextPresentPrecision("消息中心", "已进入消息中心页面...");
-		ds.mClickReturnButton();
+		//返回行情页面
+		BaseAction.mReturn();
 	}
 	
 	@Test(description = "行情页面K线选择M5")//除‘分时线’图ID不一样外，其它四个标签显示图ID都一样
 	public void testK_lineSelectM5() throws Exception{
 		//点击K线M5
-		ds.mClickKX("M5");
-		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功");
+		QuotesPage.verify().mK_M5.click();
+		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功...");
 	}
 	
 	@Test(description = "行情页面K线选择M15")//除‘分时线’图ID不一样外，其它四个标签显示图ID都一样
 	public void testK_lineSelectM15() throws Exception{
 		//点击K线M15
-		ds.mClickKX("M15");
-		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功");
+		QuotesPage.verify().mK_M15.click();
+		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功...");
 	}
 	
 	@Test(description = "行情页面K线选择H1")//除‘分时线’图ID不一样外，其它四个标签显示图ID都一样
 	public void testK_lineSelectH1() throws Exception{
 		//点击K线H1
-		ds.mClickKX("H1");
-		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功");
+		QuotesPage.verify().mK_H1.click();
+		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功...");
 	}
 	
 	@Test(description = "行情页面K线选择D1")//除‘分时线’图ID不一样外，其它四个标签显示图ID都一样
 	public void testK_lineSelectD1() throws Exception{
 		//点击K线D1
-		ds.mClickKX("D1");
-		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功");
+		QuotesPage.verify().mK_D1.click();
+		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功...");
 	}
 	
 	@Test(description = "行情页面K线选择‘分时线’")//除‘分时线’图ID不一样外，其它四个标签显示图ID都一样
 	public void testK_lineSelectTimeshare() throws Exception{
 		//点击K线D1
-		ds.mClickKX("D1");
-		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_view5_1", 3000), true, "切换成功");
+		QuotesPage.verify().mK_D1.click();
 		//点击分时线
-		ds.mClickKX("分时线");
-		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_time_hour_1", 3000), true, "切换成功");
+		QuotesPage.verify().mK_Timeshare.click();
+		Assertion.VerityBoolean(BaseAction.position.waitForElement(ElementAttribs.RESOURCE_ID, "com.ylmall.app.ui:id/newcfd_my_charts_time_hour_1", 3000), true, "切换成功...");
 	}
 	
-	@Test(description = "行情页面点击定购按钮")
+	@Test(description = "进入定购页面")
 	public void testClickOrder() throws Exception{
 		//点击定购按钮
-		QuotesPage.verify().mDingGou();
-		Assertion.VerityTextPresentPrecision("现价定购", "是否进入定购页面");
+		QuotesPage.verify().mOrderButton.click();
+		Assertion.VerityTextPresentPrecision("现价定购", "是否进入定购页面...");
 	}	
 }
